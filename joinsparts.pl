8bfcddc6b9650d586403882155632103bafca5f0
Merlijn Hofstra <mhofstra@gmail.com>
2010-01-03 14:54:39 +0100

    really fix the geo data loading

--- joinsparts.pl
+++ joinsparts.pl
@@ -15,8 +15,11 @@
 # You can obtain a copy here: http://search.cpan.org/~bricas/Geo-IPfree-0.8/lib/Geo/IPfree.pm
 # Place the 'Geo' dir in the same directory as this plugin or anywhere in @INC.
 if ($pj{irc_show_country}) {
-	$pj{geo} = Geo::IPfree->new;
-	$pj{geo}->Faster; # Due to a relatively large amount of lookups, this is probably a good idea
+	eval { 
+		require Geo::IPfree;
+		$pj{geo} = Geo::IPfree->new;
+		$pj{geo}->Faster; # Due to a relatively large amount of lookups, this is probably a good idea 
+	} or die "joinsparts.pl: requested countrynames, but can't load data, $@";
 } 
 
 $store{plugin_joinsparts} = \%pj; }
