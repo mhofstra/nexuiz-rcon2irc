1ecb3b688a80edaf06b13446d3a09315413fe4bf
Merlijn Hofstra <mhofstra@gmail.com>
2010-01-16 19:26:21 +0100

    always use movetoteam sub

--- teams.pl
+++ teams.pl
@@ -813,7 +813,7 @@ sub killdeathratio {
 		my $team = $team_code2name{$_};
 		foreach my $plrslot ( @{ $bestteams->[$_] } ) {
 			unless ($_ == $tp->{teams}->[$store{"playerid_byslot_$plrslot"}]) { # TODO uncomment
-				#out dp => 0, "movetoteam_$team " . $plrslot . " 1";
+				#movetoteam($store{"playerid_byslot_$plrslot"}, $team, 1);
 			}
 		}
 	}
@@ -936,7 +936,7 @@ sub killdeathratio {
 [ dp => q{\001(.*?)\^7: (.*)} => sub {
 	my ($nickraw, $message) = @_;
 	$message = color_dp2none $message;
-	if ($message =~ m/^!?(teams|unbalanced|unfair)[!\.,;]?$/gi) {
+	if ($message =~ m/^!?(teams|unbalanced|unfair)[!\.\?,;]?$/gi) {
 		my $nick = color_dp2irc $nickraw;
 		my $str = stats_irc();
 		out irc => 0, "PRIVMSG $config{irc_channel} :\00307* balance\017: $nick\017 thinks the teams are unfair: $str";
